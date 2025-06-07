d9164b912eeb47890badeb9adedbeb5688129582
heoiya <heoiyaerot@gmail.com>
2025-06-06 19:45:20 -0700

    Add files via upload

---
+++ components/icons/ClipboardIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ClipboardIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-3.5 h-3.5">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a2.25 2.25 0 0 1-2.25 2.25h-1.5a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 16.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75Z" />
+  </svg>
+);
---
+++ components/icons/SendIcon.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export const SendIcon: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+    <path d="M3.105 3.105a1.5 1.5 0 0 1 2.121-.188l11.455 7.998a1.5 1.5 0 0 1 0 2.568l-11.454 7.998a1.5 1.5 0 0 1-2.121-.188L.184 13.089a1.5 1.5 0 0 1 1.09-2.528l5.067-.211L1.274 7.912a1.5 1.5 0 0 1-1.09-2.528L3.105 3.105Z" />
+  </svg>
+);
