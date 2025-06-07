749de46264cc30e92d7bbb5eecc1627c5cc0396a
heoiya <heoiyaerot@gmail.com>
2025-06-06 19:44:19 -0700

    Create AiIcon.tsx

---
+++ components/icons/AiIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const AiIcon: React.FC = () => (
+ <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-7.5h10.5a2.25 2.25 0 002.25-2.25V8.25a2.25 2.25 0 00-2.25-2.25H6.75A2.25 2.25 0 004.5 8.25v3.5A2.25 2.25 0 006.75 14.25zM12 6.75h.008v.008H12V6.75zm0 3h.008v.008H12V9.75zm0 3h.008v.008H12v-.008zm3-3h.008v.008H15V9.75zm-6 0h.008v.008H9V9.75z"/>
+ </svg>
+);
